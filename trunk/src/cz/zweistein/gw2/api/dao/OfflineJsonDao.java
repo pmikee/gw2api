2b5fd60a5fe4d6e7e9434b760157fb7e4ccaa886
zwei2stein@gmail.com <zwei2stein@gmail.com@6e7069b9-7f5f-e0c9-1400-d63776639394>
2013-05-26 16:04:18 +0000

    offline dao and ability to use offline dump.

--- trunk/src/cz/zweistein/gw2/api/dao/OfflineJsonDao.java
+++ trunk/src/cz/zweistein/gw2/api/dao/OfflineJsonDao.java
@@ -1,76 +1,98 @@
 package cz.zweistein.gw2.api.dao;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.rmi.RemoteException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
 
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
 public class OfflineJsonDao implements JsonDao {
+	
+	private JSONObject recipeDetails;
+	private JSONObject items;
 
+	public OfflineJsonDao() throws FileNotFoundException, IOException, ParseException {
+		recipeDetails = (JSONObject) new JSONParser().parse(new FileReader(new File("recipe_details.json")));
+		items = (JSONObject) new JSONParser().parse(new FileReader(new File("recipe_created_items.json")));
+	}
+
+	@SuppressWarnings("unchecked")
 	@Override
 	public JSONObject getItems() throws RemoteException {
-		// TODO Auto-generated method stub
-		return null;
+		JSONObject obj = new JSONObject();
+		Set<String> ids = items.keySet();
+		List<Long> result = new ArrayList<Long>(ids.size());
+		for (String object : ids) {
+			result.add(new Long(object));
+		}
+		obj.put("items", result);
+		return obj;
 	}
 
 	@Override
 	public JSONObject getItemDetails(Long id, String lang) throws RemoteException {
-		// TODO Auto-generated method stub
-		return null;
+		return (JSONObject) items.get(id.toString());
 	}
 
+	@SuppressWarnings("unchecked")
 	@Override
 	public JSONObject getRecipes() throws RemoteException {
-		// TODO Auto-generated method stub
-		return null;
+		JSONObject obj = new JSONObject();
+		Set<String> ids = recipeDetails.keySet();
+		List<Long> result = new ArrayList<Long>(ids.size());
+		for (String object : ids) {
+			result.add(new Long(object));
+		}
+		obj.put("recipes", result);
+		return obj;
 	}
 
 	@Override
 	public JSONObject getRecipeDetails(Long id, String lang) throws RemoteException {
-		// TODO Auto-generated method stub
-		return null;
+		return (JSONObject) recipeDetails.get(id.toString());
 	}
 
 	@Override
 	public JSONObject getWvWMatches() throws RemoteException {
-		// TODO Auto-generated method stub
-		return null;
+		throw new UnsupportedOperationException();
 	}
 
 	@Override
 	public JSONObject getWvWMatchDetails(String id) throws RemoteException {
-		// TODO Auto-generated method stub
-		return null;
+		throw new UnsupportedOperationException();
 	}
 
 	@Override
 	public JSONArray getWvWObjectiveNames(String lang) throws RemoteException {
-		// TODO Auto-generated method stub
-		return null;
+		throw new UnsupportedOperationException();
 	}
 
 	@Override
 	public JSONObject getEvents(String eventId, String mapId, String worldId) throws RemoteException {
-		// TODO Auto-generated method stub
-		return null;
+		throw new UnsupportedOperationException();
 	}
 
 	@Override
 	public JSONArray getEventNames(String lang) throws RemoteException {
-		// TODO Auto-generated method stub
-		return null;
+		throw new UnsupportedOperationException();
 	}
 
 	@Override
 	public JSONArray getWorldName(String lang) throws RemoteException {
-		// TODO Auto-generated method stub
-		return null;
+		throw new UnsupportedOperationException();
 	}
 
 	@Override
 	public JSONArray getMapNames(String lang) throws RemoteException {
-		// TODO Auto-generated method stub
-		return null;
+		throw new UnsupportedOperationException();
 	}
 
 }
