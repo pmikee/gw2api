eaa51bc5abb72fc98d97f2d50e632a764a3d05e9
zwei2stein <zwei2stein@gmail.com>
2013-06-16 18:52:52 +0000

    small refactor of event tool

--- src/cz/zweistein/gw2/app/eventwatch/EventWatcher.java
+++ src/cz/zweistein/gw2/app/eventwatch/EventWatcher.java
@@ -1,34 +1,35 @@
 package cz.zweistein.gw2.app.eventwatch;
 
 import java.rmi.RemoteException;
-import java.util.List;
-import java.util.Locale;
-import java.util.Map;
 
-import cz.zweistein.gw2.api.GW2API;
-import cz.zweistein.gw2.api.dto.Event;
-import cz.zweistein.gw2.api.util.SupportedLanguage;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
 import cz.zweistein.gw2.app.eventwatch.gui.EventDialog;
 
 public class EventWatcher {
 
 	public static void main(String[] args) throws RemoteException {
 
-		GW2API api = new GW2API();
+		try {
+			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		} catch (ClassNotFoundException e1) {
+			e1.printStackTrace();
+		} catch (InstantiationException e1) {
+			e1.printStackTrace();
+		} catch (IllegalAccessException e1) {
+			e1.printStackTrace();
+		} catch (UnsupportedLookAndFeelException e1) {
+			e1.printStackTrace();
+		}
 
-		SupportedLanguage language = SupportedLanguage.resolve(Locale.getDefault().getLanguage());
-
-		Map<String, String> eventNames = api.getEventNames(language);
-		Map<Long, String> worldNames = api.getWorldNames(language);
-
-		EventDialog ed = new EventDialog(eventNames, worldNames);
+		EventWatcherModel model = new EventWatcherModel();
+		EventDialog ed = new EventDialog(model );
 
 		while (true) {
-
-			List<Event> eventState = api.getEvents(ed.getSelectedEvent(), null, ed.getSelectedWorld());
-
-			if (eventState.size() > 0) {
-				ed.setEventState(eventState.get(0).getEventState());
+			
+			if (model.getInitialized()) {
+				model.update();
 			}
 
 			try {
--- src/cz/zweistein/gw2/app/eventwatch/gui/EventDialog.java
+++ src/cz/zweistein/gw2/app/eventwatch/gui/EventDialog.java
@@ -9,7 +9,6 @@ import java.awt.event.ActionListener;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import java.util.Map;
 import java.util.Map.Entry;
 
 import javax.swing.JButton;
@@ -17,57 +16,51 @@ import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
 
-import cz.zweistein.gw2.api.dto.enums.EventState;
+import cz.zweistein.gw2.app.eventwatch.EventWatcherModel;
 
 public class EventDialog extends JFrame implements ActionListener {
 	private static final long serialVersionUID = -5505049052341677804L;
-
-	private String selectedEvent;
-	private Long selectedWorld;
-
-	private Map<String, String> eventNames;
-	private Map<Long, String> worldNames;
-
-	private EventState eventState;
 
 	private JLabel eventStatusLabel;
 
 	JComboBox eventNamesComboBox;
 	JComboBox worldNamesComboBox;
 
-	public EventDialog(Map<String, String> eventNames, Map<Long, String> worldNames) {
+	private EventWatcherModel model;
 
-		this.eventNames = eventNames;
-		this.worldNames = worldNames;
+	public EventDialog(EventWatcherModel model) {
 
-		this.eventState = EventState.INVALID;
+		this.model = model;
+		model.setGui(this);
 
 		JPanel content = new JPanel();
-		
+
 		content.setLayout(new GridBagLayout());
 
 		add(content);
 
 		eventStatusLabel = new JLabel();
-		content.add(eventStatusLabel, new GridBagConstraints(0, 0, 1, 1, 2, 2, GridBagConstraints.CENTER, GridBagConstraints.BOTH, new Insets(0, 0, 0, 0), 0, 0));
+		content.add(eventStatusLabel,
+				new GridBagConstraints(0, 0, 1, 1, 2, 2, GridBagConstraints.CENTER, GridBagConstraints.BOTH, new Insets(0, 0, 0, 0), 0, 0));
 		eventStatusLabel.setOpaque(true);
 		updateEventState();
 
-		
 		List<CodeTableItem> eventNamesEntries = new ArrayList<CodeTableItem>();
-		for (Entry<String, String> item : eventNames.entrySet()) {
+		for (Entry<String, String> item : model.getEventNames().entrySet()) {
 			eventNamesEntries.add(new CodeTableItem(item));
 		}
 		Collections.sort(eventNamesEntries, new CodeTableEntryComparator());
-		
+
 		eventNamesComboBox = new JComboBox(eventNamesEntries.toArray(new CodeTableItem[eventNamesEntries.size()]));
 		eventNamesComboBox.setRenderer(new CodeTableRenderer());
 		eventNamesComboBox.addActionListener(this);
-		content.add(eventNamesComboBox, new GridBagConstraints(0, 1, 2, 1, 1, 1, GridBagConstraints.WEST, GridBagConstraints.HORIZONTAL, new Insets(0, 0, 0, 0), 0, 0));
-		
+		content.add(eventNamesComboBox, new GridBagConstraints(0, 1, 2, 1, 1, 1, GridBagConstraints.WEST, GridBagConstraints.HORIZONTAL,
+				new Insets(0, 0, 0, 0), 0, 0));
+
 		List<CodeTableItem> worldNamesEntries = new ArrayList<CodeTableItem>();
-		for (Entry<Long, String> item : worldNames.entrySet()) {
+		for (Entry<Long, String> item : model.getWorldNames().entrySet()) {
 			worldNamesEntries.add(new CodeTableItem(item));
 		}
 		Collections.sort(worldNamesEntries, new CodeTableEntryComparator());
@@ -75,77 +68,75 @@ public class EventDialog extends JFrame implements ActionListener {
 		worldNamesComboBox = new JComboBox(worldNamesEntries.toArray(new CodeTableItem[worldNamesEntries.size()]));
 		worldNamesComboBox.setRenderer(new CodeTableRenderer());
 		worldNamesComboBox.addActionListener(this);
-		content.add(worldNamesComboBox, new GridBagConstraints(0, 2, 2, 1, 1, 1, GridBagConstraints.CENTER, GridBagConstraints.HORIZONTAL, new Insets(0, 0, 0, 0), 0, 0));
-		
+		content.add(worldNamesComboBox, new GridBagConstraints(0, 2, 2, 1, 1, 1, GridBagConstraints.CENTER, GridBagConstraints.HORIZONTAL, new Insets(0, 0, 0,
+				0), 0, 0));
+
 		JButton toggleButton = new JButton("-");
 		content.add(toggleButton, new GridBagConstraints(1, 0, 1, 1, 0, 0, GridBagConstraints.EAST, GridBagConstraints.VERTICAL, new Insets(0, 0, 0, 0), 0, 0));
 		toggleButton.addActionListener(this);
 
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 
-		selectedEvent = (String) ((CodeTableItem) eventNamesEntries.get(0)).getItem().getKey();
-		selectedWorld = (Long) ((CodeTableItem) worldNamesEntries.get(0)).getItem().getKey();
+		updateModel();
 		updateTitle();
-		
+
 		setAlwaysOnTop(true);
 		pack();
 		setVisible(true);
 
 	}
 
-	public String getSelectedEvent() {
-		return this.selectedEvent;
+	public void updateEventState() {
+		SwingUtilities.invokeLater(new Runnable() {
+
+			@Override
+			public void run() {
+				eventStatusLabel.setText(model.getEventState().toString());
+
+				switch (model.getEventState()) {
+				case INVALID:
+					eventStatusLabel.setBackground(Color.PINK);
+					break;
+				case ACTIVE:
+					eventStatusLabel.setBackground(Color.GREEN);
+					break;
+				case INACTIVE:
+					eventStatusLabel.setBackground(Color.CYAN);
+					break;
+				case PREPARATION:
+					eventStatusLabel.setBackground(Color.BLUE);
+					break;
+				case WARMUP:
+					eventStatusLabel.setBackground(Color.YELLOW);
+					break;
+				case SUCCESS:
+					eventStatusLabel.setBackground(Color.LIGHT_GRAY);
+					break;
+				case FAIL:
+					eventStatusLabel.setBackground(Color.RED);
+					break;
+				default:
+					break;
+				}
+			}
+		});
 	}
 
-	public Long getSelectedWorld() {
-		return this.selectedWorld;
-	}
-
-	public void setEventState(EventState eventState) {
-		this.eventState = eventState;
-		updateEventState();
-	}
-
-	private void updateEventState() {
-		eventStatusLabel.setText(this.eventState.toString());
-		
-		switch (this.eventState) {
-		case INVALID:
-			eventStatusLabel.setBackground(Color.PINK);
-			break;
-		case ACTIVE:
-			eventStatusLabel.setBackground(Color.GREEN);
-			break;
-		case PREPARATION:
-			eventStatusLabel.setBackground(Color.BLUE);
-			break;
-		case WARMUP:
-			eventStatusLabel.setBackground(Color.YELLOW);
-			break;
-		case SUCCESS:
-			eventStatusLabel.setBackground(Color.LIGHT_GRAY);
-			break;
-		case FAIL:
-			eventStatusLabel.setBackground(Color.RED);
-			break;
-		default:
-			break;
-		}
-	}
-	
 	private void updateTitle() {
-		setTitle(eventNames.get(selectedEvent) + " @ " + worldNames.get(selectedWorld));
+		setTitle(model.getEventNames().get(model.getSelectedEvent()) + " @ " + model.getWorldNames().get(model.getSelectedWorld()));
+	}
 
+	public void updateModel() {
+		model.setSelectedEvent((String) ((CodeTableItem) eventNamesComboBox.getSelectedItem()).getItem().getKey());
+		model.setSelectedWorld((Long) ((CodeTableItem) worldNamesComboBox.getSelectedItem()).getItem().getKey());
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent ae) {
 		if (ae.getSource() == eventNamesComboBox) {
-			selectedEvent = (String)((CodeTableItem) eventNamesComboBox.getSelectedItem()).getItem().getKey();
-			this.eventState = EventState.INVALID;
+			model.setSelectedEvent((String) ((CodeTableItem) eventNamesComboBox.getSelectedItem()).getItem().getKey());
 		} else if (ae.getSource() == worldNamesComboBox) {
-			selectedWorld = (Long)((CodeTableItem) worldNamesComboBox.getSelectedItem()).getItem().getKey();
-			this.eventState = EventState.INVALID;
+			model.setSelectedWorld((Long) ((CodeTableItem) worldNamesComboBox.getSelectedItem()).getItem().getKey());
 		} else {
 			eventNamesComboBox.setVisible(!eventNamesComboBox.isVisible());
 			worldNamesComboBox.setVisible(!worldNamesComboBox.isVisible());
