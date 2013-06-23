612b49475b182ed7e3e51a5b9a03599bc045bd88
zwei2stein <zwei2stein@gmail.com>
2013-06-23 14:07:40 +0000

    Continents API

--- src/cz/zweistein/gw2/api/dao/OfflineJsonDao.java
+++ src/cz/zweistein/gw2/api/dao/OfflineJsonDao.java
@@ -143,4 +143,9 @@ public class OfflineJsonDao implements JsonDao {
 		throw new UnsupportedOperationException();
 	}
 
+	@Override
+	public JSONObject getContinents(String translateLang) throws RemoteException {
+		throw new UnsupportedOperationException();
+	}
+
 }
